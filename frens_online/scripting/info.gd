b92cb5d04aab09078b983f43e1fe7727b7c31198
MrFlutters <DeriousHD@gmail.com>
2020-06-28 10:58:23 +0100

    Fix Build Version, and make it easier for future updates.

--- frens_online/scripting/info.gd
+++ frens_online/scripting/info.gd
@@ -1,15 +1,10 @@
 extends Node
 
 func _ready():
-	var export_config = ConfigFile.new()
-	var export_config_path = "res://export_presets.cfg"
-	var config_error = export_config.load(export_config_path)
-	var app_name = export_config.get_value("preset.0.options", "application/product_name")
-	var version = export_config.get_value("preset.0.options", "application/product_version")
 	
 	print("\n[==INFO==]")
 	print("Operating System: ", OS.get_name())
-	print("Build: ",version)
+	print("Build: ",ProjectSettings.get_setting("application/config/version"))
 	if OS.get_cmdline_args().size() > 0:
 		print("CommandLine Args: ", OS.get_cmdline_args())
 	if OS.is_debug_build():
